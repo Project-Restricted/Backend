d97b95e51c884cf23ca943498f3797a52739d0e0
kirys <kiryska007@gmail.com>
2025-11-27 17:00:49 +0300

    "Изменил settings.py для настройки на сервере"

--- cinemaportal/cinemaportal/settings.py
+++ cinemaportal/cinemaportal/settings.py
@@ -156,19 +156,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+from pathlib import Path
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-import os
-
-if os.environ.get('RENDER'):
-    DEBUG = False
-    ALLOWED_HOSTS = ['*']
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
