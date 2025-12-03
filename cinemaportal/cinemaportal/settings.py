cc648a6a2c1d1d40754341e48a25f61eb83faff8
kirys <kiryska007@gmail.com>
2025-12-03 21:31:08 +0300

    щас точно задеплою

--- cinemaportal/cinemaportal/settings.py
+++ cinemaportal/cinemaportal/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--74y@d8djp88lu@gx-w+xqh0%#ja%4_oh3@41&eo)b1*h_ijb('
+SECRET_KEY = config('SECRET_KEY', default='django-insecure--74y@d8djp88lu@gx-w+xqh0%#ja%4_oh3@41&eo)b1*h_ijb(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*', '.tunnel4.com', 'f104528d-c2fa-4aeb-9821-72f1292519bc.tunnel4.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
@@ -149,12 +150,12 @@ WSGI_APPLICATION = 'cinemaportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cinemaportal',
-        'USER': 'cinema_user',
-        'PASSWORD': 'localpass123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': config('DB_NAME', default=str(BASE_DIR / 'db.sqlite3')),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default=''),
     }
 }
 
